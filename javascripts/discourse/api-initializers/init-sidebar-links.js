16394f882010c28078b60adfcfc49b3936ca36a6
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-03-13 21:12:39 +1300

    DEV: Move JS logic to dedicated file

---
+++ javascripts/discourse/api-initializers/init-sidebar-links.js
@@ -0,0 +1,63 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("0.8.18", (api) => {
+  const links = [
+      // FontAwesome icons may need to be added in the site settings if they don't correctly appear
+      { title: "Users Map (NEW)", src: "/u/user-map", icon: settings.icon }
+  ]
+
+  $(document).ready(function () {
+      if (document.getElementById("toggle-hamburger-menu")) {
+          // We're in mobile view
+          addToggleListener(document.getElementById("toggle-hamburger-menu"))
+      } else {
+          // We're in desktop view
+          addToggleListener(document.getElementsByClassName("btn-sidebar-toggle")[0])
+          addCustomLinks()
+      }
+
+      function addToggleListener(toggleEl) {
+          if (toggleEl) {
+             toggleEl.addEventListener("click", function () {
+                 // Wait a bit for the sidebar to load
+                 setTimeout(function() {
+                     let sidebar = document.getElementsByClassName("sidebar-section-header").length
+                     if (sidebar) {
+                          addCustomLinks()
+                      }
+                  }, 100)
+              })
+          }
+      }
+    
+      function addCustomLinks() {
+          // Wait a bit until navigation has been loaded
+          setTimeout(function () {
+              const parentEl = document.getElementsByClassName("sidebar-section-content")[0]
+              let linksAlreadyAdded = document.getElementsByClassName("sidebar-section-custom-link").length
+        
+              if (parentEl && !linksAlreadyAdded) {
+                  links.filter(function (link) {
+                      let linkDiv = document.createElement("li")
+                      let linkTitleTrim = link.title.replace(/\s+/g, '')
+                      linkDiv.className = "sidebar-section-link-wrapper sidebar-section-custom-link"
+                      linkDiv.innerHTML = `<a href="${link.src}" class="sidebar-section-link sidebar-section-link-everything sidebar-row ember-view">
+                            <span class="sidebar-section-link-prefix icon" id="link_${linkTitleTrim}"></span>
+                            <span class="sidebar-section-link-content-text"> ${link.title} </span>
+                        </a>
+                      `
+                      parentEl.append(linkDiv)
+                    
+                      let linkIcon = document.getElementById("link_" + linkTitleTrim)
+                      if (linkIcon && link.icon) {
+                          linkIcon.innerHTML = `<svg viewBox="0 0 640 512" class="fa d-icon svg-icon prefix-icon svg-string d-icon-${link.icon}" xmlns="http://www.w3.org/2000/svg">
+                                  <use xlink:href="#${link.icon}"></use>
+                              </svg>
+                        `
+                      }
+                 })
+              }
+         }, 100)
+      }
+  });
+});
